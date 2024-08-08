b92679fee5cdfc6f2ab6c818a44e19a7ac6082fd
Rachnera <arachne.silk@gmail.com>
2024-08-08 16:13:58 +0200

    Use a custom font for main menu

--- CustomTitleScreen.rb
+++ CustomTitleScreen.rb
@@ -1,7 +1,26 @@
+module CustomTitleScreen
+  CONFIG = {
+    font: {
+      name: "Montserrat Black",
+    }
+  }
+end
+
 class Scene_Title < Scene_Base
   alias original_879_create_command_window create_command_window
   def create_command_window
     original_879_create_command_window
     @command_window.windowskin = Cache.system("TitleScreen")
+  end
+end
+
+class Window_TitleCommand < Window_Command
+  def draw_text(*args)
+    old_f = contents.font.name
+    contents.font.name = CustomTitleScreen::CONFIG[:font][:name]
+
+    contents.draw_text(*args)
+
+    contents.font.name = old_f
   end
 end
---
+++ README.md
@@ -0,0 +1,15 @@
+## Setup
+
+- Copy the `TitleScreen.png` file to the `Graphics/System/` folder
+- Copy the `Fonts/` folder to the root of the project
+- Add the `CustomTitleScreen.rb` file as a new script
+
+## How to
+
+### Change the font used
+
+- Drop the file(s) of the new font you wish to use in the `Fonts/` folder of the game
+- Change the `font` -> `name` variable in the config of the script
+
+See [there](https://forums.rpgmakerweb.com/index.php?threads/how-do-i-font-change-vx-ace.136293/#post-1186160) for help with debugging some common issues (typo, folder hierarchy, etc.)
+
